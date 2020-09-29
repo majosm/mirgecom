ea191ea579435c6dbd7107ca6db9f8eaed2ea01b
Matthew Smith <mjsmith6@illinois.edu>
2020-09-29 10:11:24 -0500

    first attempt at heat equation (doesn't work)

--- mirgecom/heat.py
+++ mirgecom/heat.py
@@ -1,10 +1,16 @@
-""":mod:`mirgecom.wave` computes the rhs of the wave equation.
+r""":mod:`mirgecom.heat` computes the rhs of the heat equation.
 
-.. autofunction:: wave_operator
+Heat equation:
+
+.. math::
+
+    \partial_t \mathbf{u} = \alpha\nabla^2\mathbf{u}
+
+.. autofunction:: heat_operator
 """
 
 __copyright__ = """
-Copyright (C) 2020 University of Illinois Board of Trustees"
+Copyright (C) 2020 University of Illinois Board of Trustees
 """
 
 __license__ = """
@@ -27,6 +33,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+import math
 import numpy as np
 import numpy.linalg as la  # noqa
 from pytools.obj_array import (
@@ -36,38 +43,35 @@ from meshmode.dof_array import thaw
 from grudge.symbolic.primitives import TracePair
 from grudge.eager import interior_trace_pair, cross_rank_trace_pairs
 
-
-def _flux(discr, c, w_tpair):
-    u = w_tpair[0]
-    v = w_tpair[1:]
-
+def _q_flux(discr, alpha, w_tpair):
     actx = w_tpair.int[0].array_context
 
     normal = thaw(actx, discr.normal(w_tpair.dd))
 
-    flux_weak = flat_obj_array(
-        np.dot(v.avg, normal),
-        normal*make_obj_array([u.avg]),
-        )
+    flux_weak = math.sqrt(alpha)*w_tpair.avg*normal
 
-    # upwind
-    flux_weak += flat_obj_array(
-        0.5*(u.ext-u.int),
-        0.5*normal*make_obj_array([np.dot(normal, v.ext-v.int)]),
-        )
-
-    return discr.project(w_tpair.dd, "all_faces", c*flux_weak)
+    return discr.project(w_tpair.dd, "all_faces", flux_weak)
 
 
-def wave_operator(discr, c, w):
-    """Compute the RHS of the wave equation.
+def _flux(discr, alpha, q_tpair):
+    actx = q_tpair.int[0].array_context
+
+    normal = thaw(actx, discr.normal(q_tpair.dd))
+
+    flux_weak = math.sqrt(alpha)*make_obj_array([np.dot(q_tpair.avg, normal)])
+
+    return discr.project(q_tpair.dd, "all_faces", flux_weak)
+
+
+def heat_operator(discr, alpha, w):
+    """Compute the RHS of the heat equation.
 
     Parameters
     ----------
     discr: grudge.eager.EagerDGDiscretization
         the discretization to use
-    c: float
-        the (constant) wave speed
+    alpha: float
+        the (constant) diffusivity
     w: numpy.ndarray
         an object array of DOF arrays, representing the state vector
 
@@ -77,27 +81,28 @@ def wave_operator(discr, c, w):
         an object array of DOF arrays, representing the ODE RHS
     """
     u = w[0]
-    v = w[1:]
 
-    dir_u = discr.project("vol", BTAG_ALL, u)
-    dir_v = discr.project("vol", BTAG_ALL, v)
-    dir_bval = flat_obj_array(dir_u, dir_v)
-    dir_bc = flat_obj_array(-dir_u, dir_v)
+    actx = u.array_context
+
+    grad_u = discr.weak_grad(u)
+    q_flux = (_q_flux(discr, alpha=alpha, w_tpair=interior_trace_pair(discr, w)) +
+                sum(
+                    _q_flux(discr, alpha=alpha, w_tpair=tpair)
+                    for tpair in cross_rank_trace_pairs(discr, w)
+                )
+            )
+
+    q = discr.inverse_mass(-math.sqrt(alpha)*grad_u + discr.face_mass(q_flux))
 
     return (
         discr.inverse_mass(
-            flat_obj_array(
-                -c*discr.weak_div(v),
-                -c*discr.weak_grad(u)
-                )
+            make_obj_array([-math.sqrt(alpha)*discr.weak_div(q)])
             +  # noqa: W504
             discr.face_mass(
-                _flux(discr, c=c, w_tpair=interior_trace_pair(discr, w))
-                + _flux(discr, c=c,
-                    w_tpair=TracePair(BTAG_ALL, interior=dir_bval, exterior=dir_bc))
+                _flux(discr, alpha=alpha, q_tpair=interior_trace_pair(discr, q))
                 + sum(
-                    _flux(discr, c=c, w_tpair=tpair)
-                    for tpair in cross_rank_trace_pairs(discr, w))
+                    _flux(discr, alpha=alpha, q_tpair=tpair)
+                    for tpair in cross_rank_trace_pairs(discr, q))
                 )
             )
         )
