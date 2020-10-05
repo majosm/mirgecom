8718bbecd248c8e90abeffac4a18daee08f8001c
Matthew Smith <mjsmith6@illinois.edu>
2020-10-05 14:44:05 -0700

    add timers to lump-mpi for scaling measurements

--- examples/lump-mpi.py
+++ examples/lump-mpi.py
@@ -55,29 +55,38 @@ logger = logging.getLogger(__name__)
 
 
 def main(ctx_factory=cl.create_some_context):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    from pampr import Profiler
+    profiler = Profiler(comm)
+    profiler.start_sync("All")
+
+    profiler.start_sync("CreateContext")
     cl_ctx = ctx_factory()
     queue = cl.CommandQueue(cl_ctx)
     actx = PyOpenCLArrayContext(queue,
             allocator=cl_tools.MemoryPool(cl_tools.ImmediateAllocator(queue)))
+    profiler.stop("CreateContext")
 
     dim = 3
     nel_1d = 16
     order = 3
     exittol = .09
-    t_final = 0.01
+    t_final = 0.001
     current_cfl = 1.0
     vel = np.zeros(shape=(dim,))
     orig = np.zeros(shape=(dim,))
     vel[:dim] = 1.0
-    current_dt = .001
+    current_dt = .00002
     current_t = 0
     eos = IdealSingleGas()
     initializer = Lump(numdim=dim, center=orig, velocity=vel)
     casename = "lump"
     boundaries = {BTAG_ALL: PrescribedBoundary(initializer)}
     constant_cfl = False
-    nstatus = 1
-    nviz = 1
+    nstatus = -1
+    nviz = -1
     rank = 0
     checkpoint_t = current_t
     current_step = 0
@@ -85,21 +94,25 @@ def main(ctx_factory=cl.create_some_context):
     box_ll = -5.0
     box_ur = 5.0
 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
+    print(f"nsteps = {(t_final-current_t)/current_dt}")
 
+    profiler.start("GenerateMesh")
     from meshmode.mesh.generation import generate_regular_rect_mesh
     generate_grid = partial(generate_regular_rect_mesh, a=(box_ll,) * dim,
                             b=(box_ur,) * dim, n=(nel_1d,) * dim)
     local_mesh, global_nelements = create_parallel_grid(comm, generate_grid)
     local_nelements = local_mesh.nelements
+    profiler.stop("GenerateMesh")
 
+    profiler.start_sync("CreateDiscretization")
     discr = EagerDGDiscretization(
         actx, local_mesh, order=order, mpi_communicator=comm
     )
     nodes = thaw(actx, discr.nodes())
     current_state = initializer(0, nodes)
+    profiler.stop("CreateDiscretization")
 
+    profiler.start_sync("InitializeSim")
     visualizer = make_visualizer(discr, discr.order + 3
                                  if discr.dim == 2 else discr.order)
     initname = initializer.__class__.__name__
@@ -128,12 +141,31 @@ def main(ctx_factory=cl.create_some_context):
                               t=t, dt=dt, nstatus=nstatus, nviz=nviz,
                               exittol=exittol, constant_cfl=constant_cfl, comm=comm)
 
+    profiler.stop("InitializeSim")
+    profiler.start_sync("Dummy")
+#     n_dummy_steps, _, _ = advance_state(rhs=my_rhs, timestepper=timestepper,
+#                   checkpoint=my_checkpoint,
+#                   get_timestep=get_timestep, state=current_state,
+#                   t=current_t, t_final=current_t + 0.1*(t_final-current_t))
+#     assert n_dummy_steps >= 5
+    dummy_t = current_t
+    for i in range(5):
+        current_state = timestepper(state=current_state, t=dummy_t, dt=current_dt, rhs=my_rhs)
+        dummy_t += current_dt
+    profiler.stop("Dummy")
+    profiler.start_sync("RunSim")
     try:
-        (current_step, current_t, current_state) = \
-            advance_state(rhs=my_rhs, timestepper=timestepper,
-                          checkpoint=my_checkpoint,
-                          get_timestep=get_timestep, state=current_state,
-                          t=current_t, t_final=t_final)
+#         (current_step, current_t, current_state) = \
+#             advance_state(rhs=my_rhs, timestepper=timestepper,
+#                           checkpoint=my_checkpoint,
+#                           get_timestep=get_timestep, state=current_state,
+#                           t=current_t, t_final=t_final)
+        t = current_t
+        while t < t_final:
+            current_state = timestepper(state=current_state, t=t, dt=current_dt, rhs=my_rhs)
+            t += current_dt
+            current_step += 1
+        current_t = t
     except ExactSolutionMismatch as ex:
         current_step = ex.step
         current_t = ex.t
@@ -148,6 +180,10 @@ def main(ctx_factory=cl.create_some_context):
 
     if current_t - t_final < 0:
         raise ValueError("Simulation exited abnormally")
+
+    profiler.stop("RunSim")
+    profiler.stop("All")
+    profiler.write_reduced()
 
 
 if __name__ == "__main__":
