c87c79b578269ae030606796257065f054068339
Matthew Smith <mjsmith6@illinois.edu>
2024-06-14 10:30:42 -0500

    temp - undo formatting change

--- mirgecom/navierstokes.py
+++ mirgecom/navierstokes.py
@@ -562,10 +562,10 @@ def ns_operator(dcoll, gas_model, state, boundaries, *, time=0.0,
         vol_term = vol_term - inviscid_flux(state=vol_state_quad)
         vol_term = force_materialize(actx, vol_term)
         bnd_term = bnd_term - inviscid_flux_on_element_boundary(
-                dcoll, gas_model, boundaries, inter_elem_bnd_states_quad,
-                domain_bnd_states_quad, quadrature_tag=quadrature_tag,
-                numerical_flux_func=inviscid_numerical_flux_func, time=time,
-                dd=dd_vol)
+            dcoll, gas_model, boundaries, inter_elem_bnd_states_quad,
+            domain_bnd_states_quad, quadrature_tag=quadrature_tag,
+            numerical_flux_func=inviscid_numerical_flux_func, time=time,
+            dd=dd_vol)
         bnd_term = force_materialize(actx, bnd_term)
 
     ns_rhs = div_operator(dcoll, dd_vol_quad, dd_allfaces_quad, vol_term, bnd_term)
